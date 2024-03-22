b6d9b3426d140f4c593fe3c8728284604c744483
Rick Jonkman <rickjonkman@MacBook-Air-van-Rick.local>
2024-03-22 14:48:32 +0100

    updated

--- src/context/GroceriesContext.jsx
+++ src/context/GroceriesContext.jsx
@@ -6,45 +6,13 @@ export const GroceriesContext = createContext({ });
 
 const GroceriesContextProvider = ({ children }) => {
 
-    const groceriesFromLocalStorage = JSON.parse(localStorage.getItem('currentGroceries'));
-    const [currentGroceries, setCurrentGroceries] = useState(groceriesFromLocalStorage || []);
-    const [grocery, setGrocery] = useState({
-        id: 0,
-        name: '',
-        inStock: false,
-        notFound: false,
-    });
 
-    useEffect(() => {
-        localStorage.setItem('currentGroceries', JSON.stringify(currentGroceries));
-
-        return console.log('GroceriesContextProvider mounted');
-    }, [currentGroceries]);
-
-    const handleClearList = () => {
-        setCurrentGroceries([]);
-    }
-
-    const handleToggleGrocery = (id) => {
-        const newList = currentGroceries.map((grocery) => {
-            if (grocery.id === id) {
-                grocery.checked = !grocery.checked;
-            }
-            return grocery;
-        });
-        setCurrentGroceries(newList);
-    }
 
 
 
 
     const groceriesObject = {
-        currentGroceries,
-        setCurrentGroceries,
-        grocery,
-        setGrocery,
-        handleClearList,
-        handleToggleGrocery,
+
     }
 
     return (
--- src/pages/authenticated/groceries-pages/ShoppingModePage.jsx
+++ src/pages/authenticated/groceries-pages/ShoppingModePage.jsx
@@ -4,15 +4,47 @@ import NavBarMain from "../../../components/ui/navigation/nav--main/NavBarMain.j
 import HamIcon from "../../../assets/icons/hamburger-blue.svg";
 import Main from "../../../components/structure/Main.jsx";
 import "/src/scss/scss-pages/groceries.scss";
-import PageTitle from "../../../components/page-components/page-title/PageTitle.jsx";
-import GroceriesMenu from "../../../components/page-components/groceries/shopping-mode/GroceriesMenu.jsx";
-import ShoppingList from "../../../components/page-components/groceries/shopping-mode/ShoppingList.jsx";
 import GroceriesContextProvider from "../../../context/GroceriesContext.jsx";
 import Footer from "../../../components/structure/Footer.jsx";
+import PageTitle from "../../../components/page-components/page-title/PageTitle.jsx";
+import {useEffect, useState} from "react";
+import SubmitButton from "../../../components/ui/buttons/SubmitButton.jsx";
 
 
 const ShoppingModePage = () => {
 
+    const initialItem = [
+        {
+            id: 100,
+            name: "Productnaam",
+            inStorage: false,
+            notFound: false,
+        },
+        {
+            id: 101,
+            name: "Productnaam",
+            inStorage: false,
+            notFound: false,
+        }
+    ]
+
+    const listFromLocalStorage = localStorage.getItem("currentList");
+
+    const [currentList, setCurrentList] = useState(JSON.parse(listFromLocalStorage) || [initialItem]);
+    const [productItem, setProductItem] = useState({
+        id: 0,
+        name: "",
+        inStorage: false,
+        notFound: false,
+    })
+
+    useEffect(() => {
+        localStorage.setItem("currentList", JSON.stringify(currentList));
+    }, [currentList]);
+
+    const handleAddProduct = (product) => {
+        setCurrentList([...currentList, product]);
+    }
 
     return (
         <GroceriesContextProvider>
@@ -24,21 +56,69 @@ const ShoppingModePage = () => {
 
                 <Main>
 
+                    <PageTitle
+                        pageTitleClass="groceries-shopping-mode__title"
+                        pageTitle="Winkelmodus"
+                    />
 
-                    <PageTitle pageTitleClass='groceries-shopping-mode__title' pageTitle="Winkelmodus"/>
+                    <div className="groceries-shopping-mode__container">
 
-                    <GroceriesContextProvider>
-                        <GroceriesMenu/>
+                        <aside className="groceries-shopping-mode__menu">
 
+                            <div className="form__add-item--container">
+                                <form className="shopping-mode__form--add-item" onSubmit={() => handleAddProduct(productItem)}>
+                                    <label htmlFor="form__add-item--text-input">Voeg product toe</label>
+                                    <input
+                                        type="text"
+                                        id="form__add-item--text-input"
+                                        name="search"
+                                        placeholder="Type product..."
+                                        value={productItem.name}
+                                        onChange={(e) => setProductItem({
+                                            id: Math.floor(Math.random() * 1000),
+                                            name: e.target.value,
+                                            inStorage: false,
+                                            notFound: false,
+                                        })}
+                                    />
 
-                        <ShoppingList shoppingListTitle="Huidige lijst"/>
-                    </GroceriesContextProvider>
+                                    <SubmitButton
+                                        buttonClass="form__add-item--submit-button"
+                                        buttonText="Voeg toe"
+                                    />
+                                </form>
+                            </div>
+
+                        </aside>
+
+                        <section className="groceries-shopping-mode__content">
+                            <ul>
+                            {
+                                currentList.map((product) => (
+                                    <li key={product.id}>
+
+                                        <div className="shopping-mode__content--checkbox">
+                                            <input type="checkbox" id={product.id} name={product.name} value={product.name} />
+                                            <label htmlFor={product.id}>{product.name}</label>
+                                        </div>
+
+                                        <div className="shopping-mode__content--buttons">
+
+                                        </div>
+
+                                    </li>
+                                ))
+                            }
+                            </ul>
+                        </section>
+
+                    </div>
 
                 </Main>
 
             </OuterContainer>
 
-            <Footer />
+            <Footer/>
         </GroceriesContextProvider>
     )
         ;
